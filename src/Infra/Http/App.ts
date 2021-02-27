3f194f294024f10919b6ddb7e72724671f7f0f55
mpgxc <mpgx5.c@gmail.com>
2021-02-27 16:49:29 -0300

    feat: Adicionando middlewares de segurança e logs

--- src/Infra/Http/App.ts
+++ src/Infra/Http/App.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
-import express from 'express';
 import 'express-async-errors';
+import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
 import { router } from './Routes';
 
 import '../Database';
@@ -9,7 +11,11 @@ import '../Database';
 const app = express();
 
 app.use(express.json());
+
 app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+
 app.use(router);
 
 export { app };
