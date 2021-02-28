18b678f7abea4e6958b2e202eb1dd29e457f73be
mpgxc <mpgx5.c@gmail.com>
2021-02-28 01:17:21 -0300

    feat: Exportação dos módulos

---
+++ src/Infra/Http/Factories/Controllers/index.ts
@@ -0,0 +1,4 @@
+export { Generic } from './AuthenticateUserControllerFactory';
+export { MakeListSurveyController } from './ListSurveyControllerFactory';
+export { MakeRegisterSurveyController } from './RegisterSurveyControllerFactory';
+export { MakeRegisterUserController } from './RegisterUserControllerFactory';
